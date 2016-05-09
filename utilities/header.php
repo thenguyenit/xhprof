10ee2dcc94ee02a738c41ffd1ac3c10ae7b3b199
Nguyen Nguyen <ntnguyenit@gmail.com>
2016-05-09 14:00:15 +0700

    Update header.php

--- utilities/header.php
+++ utilities/header.php
@@ -18,7 +18,7 @@ if (isset($_REQUEST['no-A9v3XUsnKX3aEiNsUDZzV'])) {
 }
 
 if ($xhprof && extension_loaded('xhprof')) {
-    include_once '/var/www/html/xhprof/xhprof_lib/utils/xhprof_lib.php';
-    include_once '/var/www/html/xhprof/xhprof_lib/utils/xhprof_runs.php';
+    include_once 'vendor/thenguyenit/xhprof/xhprof_lib/utils/xhprof_lib.php';
+    include_once 'vendor/thenguyenit/xhprof/xhprof_lib/utils/xhprof_runs.php';
     xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 }
